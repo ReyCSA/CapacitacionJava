e68945a881ead3946f55b2618b53174c2658a1c5
Rey Castro <reycsa95@gmail.com>
2023-09-29 16:40:57 -0600

    Rey|menu salir

---
+++ Ejercicio1/src/Calculadora_con_Salir.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) throws IOException {
+
+        int numero1=0, numero2=0, opcion;
+        boolean continuar = true;
+
+        //Scanner scanner = new Scanner(System.in);
+
+        BufferedReader ingresa = new BufferedReader(new InputStreamReader(System.in));
+
+        while (continuar) {
+            System.out.print("Introduce el primer numero: ");
+            numero1 = Integer.parseInt(ingresa.readLine());
+            System.out.print("Introduce el segundo numero: ");
+            numero2 = Integer.parseInt(ingresa.readLine());
+
+            System.out.println("Por favor elige la operación a realizar:");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicacion");
+            System.out.println("4. Division");
+            System.out.println("5. Salir :(");
+
+
+            opcion = Integer.parseInt(ingresa.readLine());
+            double resultado = 0;
+
+            switch (opcion) {
+                case 1:
+                    resultado = numero1 + numero2;
+                    System.out.println("El resultado de la suma es: " + resultado);
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    System.out.println("El resultado de la resta es: " + resultado);
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    System.out.println("El resultado de la multiplicación es: " + resultado);
+                    break;
+                case 4:
+                    if (numero2 != 0) {
+                        resultado = (double) numero1 / numero2;
+                        System.out.println("El resultado de la división es: " + resultado);
+                    } else {
+                        System.out.println("Error: No es posible dividir por cero.");
+                    }
+                    break;
+                case 5:
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        }
+
+        ingresa.close();
+    }
+}
